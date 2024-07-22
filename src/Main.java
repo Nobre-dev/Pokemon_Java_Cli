646e4808c4d3e95a00db7317665fccb505b07bc4
Santiago Nobre <141985429+Nobre-dev@users.noreply.github.com>
2024-07-22 15:52:43 +0100

    s

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Start
+        //Inicio
         System.out.println("Bem vindo ao pokemon 2.0 cli por favor diga seu nome:");
         Scanner scanner = new Scanner(System.in);
         String playername = scanner.nextLine();
 
-
+        //Criar o personagem
         Character player = new Character(playername,0,0,1);
 
-        System.out.println(player.Name);
+
 
 
     }
