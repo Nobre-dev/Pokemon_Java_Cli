35506b120e76fe7b96933cacdc547852510e6a0c
Santiago Nobre <141985429+Nobre-dev@users.noreply.github.com>
2024-07-22 15:48:55 +0100

    upd

---
+++ src/Character.java
@@ -0,0 +1,17 @@
+public class Character {
+
+    String Name;
+    int Level;
+    float XP;
+    int Strength;
+
+    Character(String Name, int Level, float XP, int Strength){
+        this.Name = Name;
+        this.Level = Level;
+        this.XP = XP;
+        this.Strength = Strength;
+    }
+    void Atk(){}
+    void Run(){}
+
+}
